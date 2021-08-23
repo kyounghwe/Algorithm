d1230f8e9fb31e0b7140cb2d85797a20854bd999
경회 권 <kyounghwe2544@hanmail.net>
2021-08-23 02:30:50 +0000

    3_단백질_바.py

--- 3주차 알고리즘 L1(권경회)/3_단백질_바.py
+++ 3주차 알고리즘 L1(권경회)/3_단백질_바.py
@@ -1,20 +1,19 @@
-portein = int(input())
+portein = int(input()) 
 
-peanut_protein = 0  # 땅콩
-chicken_protein = 0  # 닭
-pure_protein = 0  # 순수
+peanut_protein = 0 #땅콩
+chicken_protein = 0 #닭
+pure_protein = 0 #순수
 
-pure_protein = portein // 250
-remain_portein = portein - (pure_protein * 250)
+pure_protein =  portein // 250 
+remain_protein = portein - (pure_protein * 250)
 
-chicken_protein = remain_portein // 40
-remain_portein = remain_portein - (chicken_protein * 40)
+chicken_protein = remain_protein // 40
+remain_protein = remain_protein - (chicken_protein * 40)
 
-peanut_protein = remain_portein // 10
-remain_portein = remain_portein - (peanut_protein * 10)
+peanut_protein = remain_protein // 10
+remain_protein = remain_protein - (peanut_protein * 10)
 
-
-if remain_portein != 0:
+if remain_protein != 0:
     print("-1")
 else:
     print(peanut_protein, chicken_protein, pure_protein)
