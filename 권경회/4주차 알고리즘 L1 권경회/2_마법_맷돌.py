6d9e54ef79260b5689d97d2e91a3190632c26cda
경회 권 <kyounghwe2544@hanmail.net>
2021-08-25 09:16:32 +0000

    설명 추가

--- 권경회/4주차 알고리즘 L1 권경회/2_마법_맷돌.py
+++ 권경회/4주차 알고리즘 L1 권경회/2_마법_맷돌.py
@@ -5,13 +5,15 @@ s_string = ''
 t_string = ''
 
 result = 0
-
+# s 문자열의 길이가 50을 초과 할 때 까지 추가
 while len(s_string) < 50:
     s_string += s
 
+# t 문자열의 길이가 50을 초과 할 때 까지 추가
 while len(t_string) < 50:
     t_string += t
 
+#두 문자열의 인덱스 하나 하나씩 비교
 for i in range(0, 50):
     if(s_string[i] == t_string[i]):
         result = 1
