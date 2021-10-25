1d11557af2a3805202d51b82fdaf85a996ceeb7b
박 지용 <dankal0325@naver.com>
2021-10-25 10:01:38 +0000

    [난이도 중][이분탐색]

---
+++ 4주차 알고리즘 L1 박지용/12주차 알고리즘 L1 박지용/1026_회장님의 고민.py
@@ -0,0 +1,13 @@
+N = int(input())
+Budget = list(map(int, input().split()))
+M = int(input())
+
+if sum(Budget) <= M:
+    print(max(Budget))
+
+else:
+    M = M - min(Budget)
+    Budget.remove(min(Budget))
+    print(M//(N-1))
+
+#60점 
