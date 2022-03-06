2a96fec19e2da1fc91f16c323259bfe7fcee11d9
경회 권 <kyounghwe2544@hanmail.net>
2022-03-06 18:11:06 +0900

    gitlab to github mirror a algorithm repository

--- 송예림/3주차 알고리즘 L2 송예림/4_K_번_곱하기.py
+++
@@ -1,5 +0,0 @@
-n,k = map(int,input().split())
-sum_num =0
-for i in range(0,n+1):
-    sum_num += i ** k
-print(sum_num%1000000009)
--- 송예림/알고리즘 10월/[난이도하]도어락비밀번호.py
+++
@@ -1,8 +0,0 @@
-birth = list(map(str,input().split()))
-one = birth[0]
-two = birth[1]
-
-a = int(one[::-1])
-b = int(two[::-1])
-
-print(a+b)
